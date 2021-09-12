654ea5a5416d2c48d5e6c77921b3467723f753c6
Andresdavidala <65403321+Andresdavidala@users.noreply.github.com>
2021-09-11 23:13:59 -0500

    BD Compra
    base de datos para modulo compra

---
+++ AerolineApp/Query/CompraBD.sql
@@ -0,0 +1,22 @@
+USE Aereolinea1
+
+
+
+CREATE TABLE [dbo].[compra](
+	[Cedula] [nchar](10) NULL,
+	[Nombres] [nchar](50) NULL,
+	[Apellidos] [nchar](50) NULL,
+	[NumeroPasajeros] [int] NULL,
+	[Clase] [nchar](20) NULL,
+	[Pais] [nchar](30) NULL,
+	[Origen] [nchar](100) NULL,
+	[Destino] [nchar](100) NULL,
+	[FechaIda] [nchar] (100) NULL,
+	[FechaRegreso] [nchar] (100) NULL,
+	[ValorTotal] [float] NULL
+) ON [PRIMARY]
+
+ALTER TABLE compra
+ADD UNIQUE (Cedula)
+
+select * from compra
