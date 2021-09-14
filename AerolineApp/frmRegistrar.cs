7a398b8331dd3f88b438fa5ebc99e4e2017af331
SneyderJLO <tenemriq@outlook.com>
2021-09-14 13:39:19 -0500

    registrar

--- AerolineApp/frmRegistrar.cs
+++ AerolineApp/frmRegistrar.cs
@@ -7,14 +7,55 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using CapaLogicaNegocio;
 namespace AerolineApp
 {
     public partial class frmRegistrar : Form
     {
+        ClsUsuario usuario = new ClsUsuario();
         public frmRegistrar()
         {
             InitializeComponent();
+            txtPassword.PasswordChar = '*';
+        }
+
+        public void LimpiarCeldas()
+        {
+            txtNombre.Text = "";
+            txtPassword.Text = "";
+            txtUsuario.Text = "";
+
+
+        }
+
+     
+
+        private void btnRegistrar_Click_1(object sender, EventArgs e)
+        {
+            String msj = "";
+
+            if (txtUsuario.Text.Equals("") || txtNombre.Text.Equals("") || txtPassword.Text.Equals(""))
+            {
+                msj = "Todos los campos deben ser ingresados";
+                MessageBox.Show(msj);
+            }
+            else
+            {
+
+                try
+                {
+                    this.usuario.Nombre = txtNombre.Text;
+                    this.usuario.Usuario = txtUsuario.Text;
+                    this.usuario.Password = txtPassword.Text;
+                    msj = this.usuario.registrarUsuario(txtNombre.Text, txtUsuario.Text);
+                    MessageBox.Show(msj);
+                    LimpiarCeldas();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
         }
     }
 }
---
+++ CapaLogicaNegocio/ClsUsuario.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaAccesoDatos;
+
+
+namespace CapaLogicaNegocio
+{
+    public class ClsUsuario
+    {
+        private String nombre;
+        private String usuario;
+        private String password;
+
+        public ClsUsuario() { }
+
+        public ClsUsuario(string nombre, string usuario, string password)
+        {
+            this.nombre = nombre;
+            this.usuario = usuario;
+            this.password = password;
+        }
+
+        public String Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public String Usuario
+        {
+            get { return usuario; }
+            set { usuario = value; }
+        }
+
+        public String Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+        ClsManejadorUsuario manejadorUsuarios = new ClsManejadorUsuario();
+
+        public String registrarUsuario(String name, String user)
+        {
+            string msj = "";
+
+            List<ClsParametrosUsuario> lst = new List<ClsParametrosUsuario>();
+
+            try
+            {
+
+                lst.Add(new ClsParametrosUsuario(Nombre, Usuario, Password));
+                msj = manejadorUsuarios.InsertUsuario(lst);
+            }
+            catch (Exception ex)
+            {
+                msj = "Error al insertar los datos";
+                throw ex;
+            }
+
+            return msj;
+        }
+
+    }
+}
+
