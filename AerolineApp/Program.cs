6a1b0cd5a2f5380b3ebd1cc3de968ed9f3bee3a7
SneyderJLO <tenemriq@outlook.com>
2021-09-14 17:22:46 -0500

    FlotaCompleto

--- AerolineApp/Program.cs
+++ AerolineApp/Program.cs
@@ -16,7 +16,7 @@ namespace AerolineApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmPrincipal());
+            Application.Run(new frmFlotaAviones());
         }
     }
 }
